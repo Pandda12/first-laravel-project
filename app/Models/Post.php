d4e678cd39aa5f9cedbdc1d081682f4190726e2b
Vladimir Lobko <vova1234lobko@gmail.com>
2024-01-07 11:57:39 +0300

    Update migrations and models

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,20 +2,10 @@
 
 namespace App\Models;
 
-use  Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
     use HasFactory;
-    use SoftDeletes;
-    protected $guarded = false;
-
-    //public $someProperty;
-
-    public function category()
-    {
-        return$this->belongsTo(Category::class, 'category_id', 'id');
-    }
 }
--- database/migrations/2024_01_07_070347_create_posts_table.php
+++ database/migrations/2024_01_07_070347_create_posts_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('post_tags', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('post_tags');
+        Schema::dropIfExists('posts');
     }
 };
